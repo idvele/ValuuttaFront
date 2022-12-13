584bba5f1c344ae3a99b532719b41b21a204b526
Ilari Katajamäki <ilari.katajamaki1@gmail.com>
2022-12-13 16:10:41 +0200

    Just add logic

--- src/entrybox.js
+++ src/entrybox.js
@@ -1,13 +1,15 @@
 
 function Entrybox(){
     return(
-        <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Entrybox</h5>
-          
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
+        <div>
+          <h4>Amount</h4>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text">$</span>
+            </div>
+            <input type="text" className="CurrencyAmount"></input>
+    
+          </div>
         </div>
     )
 }
--- src/newrate.js
+++ src/newrate.js
@@ -3,9 +3,23 @@ function newrate(){
     return (
         <div class="card">
         <div class="card-body">
-          <h5 class="card-title">Newrate</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <h5 class="card-title">Create new rate</h5>
+          <div className="newInputs">
+          <div class="input-group">
+
+  <div class="input-group-prepend">
+    <span class="input-group-text">name</span>
+  </div>
+  <input id="newName"></input>
+
+  <div class="input-group-prepend">
+    <span class="input-group-text">Rate</span>
+  </div>
+  <input id="newRate"></input>
+</div>
+
+          </div>
+          <a href="#" class="btn btn-primary">Create</a>
         </div>
         </div>
     );
