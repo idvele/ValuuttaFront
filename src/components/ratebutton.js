f6664329433b3c01ee18c0147bc33fac41c04fa1
Ilari Katajamäki <ilari.katajamaki1@gmail.com>
2022-12-16 14:31:47 +0200

    finished ratebutton and lost apiget

--- src/components/ratebutton.js
+++ src/components/ratebutton.js
@@ -1,32 +1,40 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
-   import axios from "axios";
-   import { useEffect, useState } from "react";
 
 
    
    function Ratebutton(props){
        //kaikkien nappuloiden renderöinti tapahtuu tässä elementissä
 
-       let rateButtonAmount
-       useEffect(()=>{
-            const getRateButtons= async()=>{
-              const url = 'https://localhost:7207/Valuutta';
-              const response = await axios.get(url);
-              rateButtonAmount = response.data
+       const [rates, setRates]= useState({});
+       const [isLoading, setLoading]= useState(true);
 
-            }
-              try{ getRateButtons()
-              }
-              catch(error){
-                  console.error(error)
-                }
-                
-            },[]);
-            
-            console.log(rateButtonAmount)
-     
-    return (
-        <button type="button" className="btn btn-info" onClick={props.onClick}>GBP</button>
-    );
+       useEffect(()=>{
+        getRateButtons();
+       })
+             function getRateButtons(){
+              const url = 'https://localhost:7207/Valuutta';
+              axios.get(url).then((response)=>{
+                setRates(response.data);
+                setLoading(false);
+              });   
+            };
+               
+    
+       if (isLoading){
+        return(
+            <div>Loading...</div>
+        )
+       }
+       return(
+        <div>
+            {rates.map(rates=>
+             <button type="button" className="btn btn-info" onClick={props.onClick}>{rates.currency}</button>
+                )}
+            </div>
+       );
+    
 }
 export default Ratebutton;
